55e7d4e8c1140b389129c85f6b9363a8e249c238
Sergei <37381458+NScareN@users.noreply.github.com>
2018-06-09 23:43:05 +0700

    Add files via upload

---
+++ Modules/MyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace Helpot.Modules
+{
+    public class MyInfo : ModuleBase<SocketCommandContext>
+    {
+        [Command("myinfo")]
+        public async Task Myinfo()
+        {
+            var embed = new EmbedBuilder();
+            embed.WithTitle("Информация об аккаунте " + Context.User.Username);
+            embed.WithDescription("Имя аккаунта : " + Context.User.Username +
+                "\nId Аккаунта : " + Context.User.Id +
+                "\nДопольнительная информация : Пуша пидор");
+            embed.WithColor(new Color(0, 255, 0));
+
+            await Context.Channel.SendMessageAsync("", false, embed);
+        }
+    }
+}
